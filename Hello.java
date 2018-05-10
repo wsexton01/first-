07dfadc16c0200b9cf103216ad816419579f5ead
wsexton01 <39155141+wsexton01@users.noreply.github.com>
2018-05-10 10:57:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("******");
+		System.out.println("How Old Are You");
+		Scanner scnr = new Scanner(System.in);
+		int age= scnr.nextInt();
+		System.out.println(age);
+		int Age = 19;
+		System.out.print(65 - Age);
+	}
+
+}
---
+++ Newjava.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Newjava {
+	                      
+		 public static void main (String [] args) { 
+			 System.out.println("Type 2 numbers");
+			 Scanner scnr = new Scanner(System.in);
+			 int Num1;
+			 int Num2;
+			 Num1 = scnr.nextInt();
+			 Num2 = scnr.nextInt();
+			
+					 
+			 System.out.println(Num1 + Num2);
+			 System.out.println(Num1 - Num2);
+			 System.out.println(Num1 * Num2);
+			 System.out.println(Num1 / Num2);
+			 
+		      
+
+}
+}
